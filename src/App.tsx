cd27cdc0c71bbbec2818d9602b862a9d2b16dfe0
Kirill Makhnev <kalorigen@gmail.com>
2024-02-02 10:25:50 +0300

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import style from './App.module.scss';
+
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
+
+const tg = window.Telegram.WebApp;
 
 function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='content'>
+        <button onClick={onClose}>Закрыть</button>
+      </div>
     </div>
   );
 }
