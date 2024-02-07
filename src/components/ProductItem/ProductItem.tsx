15df9783a1dcc216d20529f3742b82ec9e71ad31
Kirill Makhnev <kalorigen@gmail.com>
2024-02-07 11:55:48 +0300

    added product list

---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,30 @@
+import Button from '../Button';
+import style from './ProductItem.module.scss';
+
+const ProductItem = ({ className, product, onAdd }: any) => {
+  const onAddHandLen = () => {
+    onAdd(product);
+  };
+
+  return (
+    <div className={style.item}>
+      <div className={'product ' + className}>
+        <div className={' img'} />
+        <div className={'title'}>{product.title}</div>
+        <div className={'description'}>{product.description}</div>
+        <div className={' price'}>
+          <span>
+            Стоимость: <b>{product.pricet}</b>
+          </span>
+        </div>
+        <Button
+          className={'add-btn'}
+          onClick={onAddHandLen}
+          name='Добавить в корзину'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,7 +1,102 @@
+import { useState } from 'react';
+import ProductItem from '../ProductItem/ProductItem';
 import style from './ProductList.module.scss';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const products = [
+  {
+    id: '1',
+    title: 'Джинсы',
+    price: 5000,
+    description: 'Синего цвета, прямые',
+  },
+  {
+    id: '2',
+    title: 'Куртк',
+    price: 12000,
+    description: 'Зеленого цвета, теплая',
+  },
+  {
+    id: '3',
+    title: 'Джинсы 2',
+    price: 5000,
+    description: 'Синего цвета, прямые',
+  },
+  {
+    id: '4',
+    title: 'Куртк 8',
+    price: 122,
+    description: 'Зеленого цвета, теплая',
+  },
+  {
+    id: '5',
+    title: 'Джинсы 3',
+    price: 5000,
+    description: 'Синего цвета, прямые',
+  },
+  {
+    id: '6',
+    title: 'Куртк 7',
+    price: 600,
+    description: 'Зеленого цвета, теплая',
+  },
+  {
+    id: '7',
+    title: 'Джинсы 4',
+    price: 5500,
+    description: 'Синего цвета, прямые',
+  },
+  {
+    id: '8',
+    title: 'Куртка 5',
+    price: 12000,
+    description: 'Зеленого цвета, теплая',
+  },
+];
+
+const getTotalPrice = (items: any) => {
+  return items.reducer((acc: any, item: any) => {
+    return (acc += item.price);
+  }, 0);
+};
 
 const ProductList = () => {
-  return <div className={style.products}></div>;
+  const [addedItems, setAddedItems] = useState([]);
+  const { tg } = useTelegram();
+
+  const onAdd = (product: any) => {
+    const alreadyAdded = addedItems.find((item: any) => item.id === product.id);
+    let newItems: any = [];
+
+    if (alreadyAdded) {
+      newItems = addedItems.filter((item: any) => item.id !== product.id);
+    } else {
+      newItems = [...addedItems, product];
+    }
+
+    setAddedItems(newItems);
+
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Buy ${getTotalPrice(newItems)}`,
+      });
+    }
+  };
+
+  return (
+    <div className={style.products}>
+      {products.map((item: any, index: any) => (
+        <ProductItem
+          product={item}
+          onAdd={onAdd}
+          className={style.product__item}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
